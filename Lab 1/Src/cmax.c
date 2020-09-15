9b4ed6675c26e27c00b108e64dd25656499f86ae
garrettkinman <garrettkinman@gmail.com>
2020-09-15 15:48:21 -0400

    implemented and timed cmax

--- Lab 1/Src/cmax.c
+++ Lab 1/Src/cmax.c
@@ -8,5 +8,13 @@
 #import "main.h"
 
 void cMax(float *array, uint32_t size, float *max, uint32_t *maxIndex) {
-	// TODO
+	(*max) = array[0];
+	(*maxIndex) = 0;
+
+	for (uint32_t i = 1; i < size; i++) {
+		if (array[i] > (*max)) {
+			(*max) = array[i];
+			(*maxIndex) = i;
+		}
+	}
 }
