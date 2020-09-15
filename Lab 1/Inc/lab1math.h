ba0261bb02b8d1326ad42c9a600655b0d56eb271
garrettkinman <garrettkinman@gmail.com>
2020-09-14 23:54:09 -0400

    added header and put TODO for implementing max function

---
+++ Lab 1/Inc/lab1math.h
@@ -0,0 +1,15 @@
+/*
+ * lab1math.h
+ *
+ *  Created on: Sep 14, 2020
+ *      Author: Wombat
+ */
+
+#ifndef INC_LAB1MATH_H_
+#define INC_LAB1MATH_H_
+
+#include "main.h"
+
+void cMax(float *array, uint32_t size, float *max, uint32_t *maxIndex);
+
+#endif /* INC_LAB1MATH_H_ */
---
+++ Lab 1/Src/cmax.c
@@ -0,0 +1,12 @@
+/*
+ * cmax.c
+ *
+ *  Created on: Sep 14, 2020
+ *      Author: Wombat
+ */
+
+#import "main.h"
+
+void cMax(float *array, uint32_t size, float *max, uint32_t *maxIndex) {
+	// TODO
+}
